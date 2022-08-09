d1814af5916a7d27578a6995279ec0e83a568fce
The Geek on Skates <geekonskates@protonmail.com>
2022-08-09 09:31:44 -0400

    Saving my progress

--- main.c
+++ main.c
@@ -1,15 +1,55 @@
 #include "main.h"
 
-int main() {
-	char buffer[80];
-	uint16_t i = 0,
-		programCounter = 0,
-		stackPointer = 0,
-		temp = 0;
-	int16_t stack[65536],
-		program[65536];
+void run(int16_t* program) {
+	int16_t stack[65536];
+	uint16_t programCounter = 0,
+		stackPointer = 0;
 	
 	memset(stack, 0, 65536 * sizeof(int16_t));
+	while(true) {
+		printf("\n\nprogramCounter = %d, stackPointer = %d\n", programCounter, stackPointer);
+		if (program[programCounter] == DONE) {
+			printf("Done.\n");
+			return;
+		}
+		if (program[programCounter] == PUSH) {
+			programCounter++;
+			stack[stackPointer] = program[programCounter];
+			printf("stack[%d] = %d\n", stackPointer, stack[stackPointer]);
+			stackPointer++;
+		}
+		else if (program[programCounter] == POP) {
+			stackPointer--;	/* Because stackPointer actually points at the position AFTER the top of the stack */
+			stack[stackPointer] = 0;
+			/* What to do with program[programCounter]; ? */
+		}
+		else if (program[programCounter] == DUP) {
+			printf("DUP (TO-DO)");
+		}
+		else if (program[programCounter] == ADD) {
+			printf("ADD (TO-DO)");
+		}
+		else if (program[programCounter] == IF_EQUAL) {
+			printf("IF_EQUAL (TO-DO)");
+		}
+		else if (program[programCounter] == JUMP) {
+			printf("JUMP (TO-DO)");
+		}
+		else if (program[programCounter] == PRINT) {
+			stackPointer--;	/* Because stackPointer actually points at the position AFTER the top of the stack */
+			printf("%d\n", stack[stackPointer]);
+			stack[stackPointer] = 0;
+			stackPointer--;
+		}
+		programCounter++;
+	}
+}
+
+int main() {
+	char buffer[80];
+	uint16_t i = 0, programCounter = 0;
+	int16_t program[65536];
+	
 	memset(program, 0, 65536 * sizeof(int16_t));
 	
 	while(true) {
@@ -93,30 +133,7 @@ int main() {
 			continue;
 		}
 		if (STRING_STARTS_WITH(buffer, "RUN")) {
-			programCounter = stackPointer = 0;
-			while(true) {
-				switch(program[programCounter]) {
-					case DONE: break;
-					case PUSH:
-						programCounter++;
-						stack[stackPointer] = program[programCounter];
-						stackPointer++;
-						break;
-					case ADD:
-						temp = stack[stackPointer];
-						stackPointer--;
-						stack[stackPointer] += temp;
-						break;
-					case PRINT:
-						stackPointer--;
-						printf("%d\n", stack[stackPointer]);
-						programCounter++;
-						break;
-					default:
-						programCounter++;
-						break;	/* For now */
-				}
-			}
+			run(program);
 			continue;
 		}
 	}
