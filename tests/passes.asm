8ea5b27a73e096661422fe7981ee161884c373af
The Geek on Skates <geekonskates@protonmail.com>
2022-08-14 17:12:41 -0400

    Added some tests
    But after talking to some of my "friends" online (I put friends in quotes just because I've never met them in person) I've decided not to do too many more.  Why?  Cuz this project is just for fun, not designed to be anything professional.  And writing tests is not as fun as building the language.  If there are bugs (which could happen even if all tests pass), they will be fixed like most bugs are: in "production". :)

---
+++ tests/passes.asm
@@ -0,0 +1,17 @@
+/*
+These are the commands I've tested so far, and pass:
+	PUSH
+	ADD #<number>
+	ADD <memory address>
+	AFS
+	DFS
+	DUP
+	POKE <memory address>
+	POP
+	RFS
+	SFS
+	PRINT
+
+
+NOTE: Multi-line comments like this are not yet supported in Cheese. :D
+*/
